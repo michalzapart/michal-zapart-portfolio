2c5727ded3b22eafe4e0cb194da011c2105b382e
admin <michal.zapart1984@gmail.com>
2026-05-26 10:41:25 +0200

    kolejna wersja

---
+++ assets/js/tailwind-config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'New-Black': '#1C1C1E',
+        'New-White': '#F5F5F7',
+        'New-Teal': {
+          DEFAULT: '#14B8A6',
+          'Intensive': '#00FFD9'
+        }
+      },
+
+    }
+  }
+}
